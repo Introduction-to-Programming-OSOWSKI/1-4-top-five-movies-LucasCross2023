ec5f7f7ef8f38d8849bedd71a12f8cebb33e3e99
LucasCross2023 <817750@mystma.org>
2020-12-18 03:28:03 +0000

    topFiveMovies try 1

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(wordOne, wordTwo, wordThree, wordFour, wordFive):
+    return wordOne + wordTwo + wordThree + wordFour + wordFive 
+
+print (topFiveMovies("End Game", "Star Wars Revenge Of The Sith", "Star Wars The Empire Strikes Back", "John Wick: Chapter 3-Parabellum", "F.B.I Guy"))
